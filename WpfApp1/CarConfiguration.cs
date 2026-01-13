9cab0af2cab61bf37a707e8dad0389cae4b7d1c0
ника <stepanecnikusa@gmail.com>
2026-01-13 10:10:43 +0300

    MainWindow.xaml.cs

---
+++ WpfApp1/CarConfiguration.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    public class CarConfig : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private string _model;
+        public string Model { get => _model; set { _model = value; OnPropertyChanged(nameof(Model)); } }
+
+        private string _engine;
+        public string Engine { get => _engine; set { _engine = value; OnPropertyChanged(nameof(Engine)); } }
+
+        private string _color;
+        public string Color { get => _color; set { _color = value; OnPropertyChanged(nameof(Color)); } }
+
+        private List<string> _options = new List<string>();
+        public List<string> Options { get => _options; set { _options = value; OnPropertyChanged(nameof(Options)); } }
+
+        private decimal _basePrice;
+        public decimal BasePrice { get => _basePrice; set { _basePrice = value; OnPropertyChanged(nameof(BasePrice)); } }
+
+        private decimal _enginePrice;
+        public decimal EnginePrice { get => _enginePrice; set { _enginePrice = value; OnPropertyChanged(nameof(EnginePrice)); } }
+
+        private decimal _colorPrice;
+        public decimal ColorPrice { get => _colorPrice; set { _colorPrice = value; OnPropertyChanged(nameof(ColorPrice)); } }
+
+        private decimal _optionsPrice;
+        public decimal OptionsPrice
+        {
+            get => _optionsPrice;
+            set
+            {
+                _optionsPrice = value;
+                OnPropertyChanged(nameof(OptionsPrice));
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+
+        public decimal TotalPrice => BasePrice + EnginePrice + ColorPrice + OptionsPrice;
+
+        private decimal _downPaymentPercent = 20;
+        public decimal DownPaymentPercent { get => _downPaymentPercent; set { _downPaymentPercent = value; OnPropertyChanged(nameof(DownPaymentPercent)); } }
+
+        private int _loanTermMonths = 36;
+        public int LoanTermMonths { get => _loanTermMonths; set { _loanTermMonths = value; OnPropertyChanged(nameof(LoanTermMonths)); } }
+
+        private decimal _loanRate = 5;
+        public decimal LoanRate { get => _loanRate; set { _loanRate = value; OnPropertyChanged(nameof(LoanRate)); } }
+
+        public decimal DownPayment => TotalPrice * (DownPaymentPercent / 100);
+        public decimal LoanAmount => TotalPrice - DownPayment;
+
+        private decimal _monthlyPayment;
+        public decimal MonthlyPayment { get => _monthlyPayment; set { _monthlyPayment = value; OnPropertyChanged(nameof(MonthlyPayment)); } }
+
+        private string _customerName;
+        public string CustomerName { get => _customerName; set { _customerName = value; OnPropertyChanged(nameof(CustomerName)); } }
+
+        private string _phone;
+        public string Phone { get => _phone; set { _phone = value; OnPropertyChanged(nameof(Phone)); } }
+
+        private string _email;
+        public string Email { get => _email; set { _email = value; OnPropertyChanged(nameof(Email)); } }
+
+        public void CalculateMonthlyPayment()
+        {
+            if (LoanAmount <= 0 || LoanTermMonths <= 0)
+            {
+                MonthlyPayment = 0;
+                return;
+            }
+
+            decimal monthlyRate = (LoanRate / 12) / 100;
+            decimal temp = (decimal)System.Math.Pow((double)(1 + monthlyRate), LoanTermMonths);
+            MonthlyPayment = LoanAmount * (monthlyRate * temp) / (temp - 1);
+        }
+    }
+
+    public static class Config
+    {
+        public static CarConfig Current { get; set; } = new CarConfig();
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfApp1;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new Step1_Model());
+        }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var currentPage = MainFrame.Content as Page;
+
+
+            if (currentPage is Step5_Order)
+            {
+                var result = MessageBox.Show(
+                    "Вы уверены, что хотите вернуться назад?\nВсе введенные данные на этой странице будут потеряны.",
+                    "Подтверждение",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+
+                    ResetStep5Data();
+
+
+                    if (MainFrame.CanGoBack)
+                        MainFrame.GoBack();
+                }
+
+            }
+            else
+            {
+
+                if (MainFrame.CanGoBack)
+                    MainFrame.GoBack();
+            }
+        }
+
+
+        private void ResetStep5Data()
+        {
+            if (Config.Current == null) return;
+
+
+            Config.Current.CustomerName = "";
+            Config.Current.Phone = "";
+            Config.Current.Email = "";
+        }
+
+        private void NextButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var currentPage = MainFrame.Content as Page;
+
+            if (currentPage is Step1_Model)
+            {
+                if (string.IsNullOrEmpty(Config.Current.Model) || string.IsNullOrEmpty(Config.Current.Engine))
+                {
+                    MessageBox.Show("Выберите модель и тип двигателя!");
+                    return;
+                }
+                MainFrame.Navigate(new Step2_ColorAndOptions());
+            }
+            else if (currentPage is Step2_ColorAndOptions)
+            {
+                if (string.IsNullOrEmpty(Config.Current.Color))
+                {
+                    MessageBox.Show("Выберите цвет автомобиля!");
+                    return;
+                }
+                MainFrame.Navigate(new Step3_Summary());
+            }
+            else if (currentPage is Step3_Summary)
+            {
+                MainFrame.Navigate(new Step4_Credit());
+            }
+            else if (currentPage is Step4_Credit)
+            {
+                MainFrame.Navigate(new Step5_Order());
+            }
+            else if (currentPage is Step5_Order)
+            {
+                var orderPage = currentPage as Step5_Order;
+                if (orderPage.ValidateData())
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            BackButton.Visibility = MainFrame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+
+            if (e.Content is Step1_Model)
+                ProgressBar.Value = 1;
+            else if (e.Content is Step2_ColorAndOptions)
+                ProgressBar.Value = 2;
+            else if (e.Content is Step3_Summary)
+            {
+                ProgressBar.Value = 3;
+                var summaryPage = e.Content as Step3_Summary;
+                summaryPage?.UpdateSummary();
+            }
+            else if (e.Content is Step4_Credit)
+            {
+                ProgressBar.Value = 4;
+                var creditPage = e.Content as Step4_Credit;
+                creditPage?.UpdateDisplay();
+            }
+            else if (e.Content is Step5_Order)
+            {
+                ProgressBar.Value = 5;
+                NextButton.Content = "Оформить заявку";
+            }
+            else
+            {
+                NextButton.Content = "Далее";
+            }
         }
     }
 }
